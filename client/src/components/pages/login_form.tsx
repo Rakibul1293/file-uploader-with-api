72b8cc1d6b78591452aa3ec1011dc64b67006c50
Rakibul1294 <md.rakibul.1294@gmail.com>
2020-09-29 01:25:00 +0600

    file upload with api

---
+++ client/src/components/pages/login_form.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useHistory } from 'react-router';
+import { Input as InputField, message } from "antd";
+import './login_form.css';
+
+const LoginForm = () => {
+	const { register, handleSubmit, control, errors } = useForm();
+	const history = useHistory();
+
+	const onSubmit = (data: any) => {
+		console.log(data);
+
+		fetch("http://localhost:5000/auth/login", {
+			method: 'POST',
+			headers: {
+				'content-Type': 'application/json'
+			},
+			body: JSON.stringify(data),
+		})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data);
+			localStorage.setItem("token", JSON.stringify(data.token));
+			localStorage.setItem("name", JSON.stringify(data.user.name));
+			if(data.message === "Auth failed") return message.error("User doesn't exist");
+			message.success('Login Successfully!');
+			history.push('/list');
+		})
+		.catch(err => {
+			console.log(err);
+			message.error(err);
+		})
+	}
+  
+    const handleRegistration = () => {
+      history.push('/registration-form');
+    }
+
+	return (
+		<div>
+			<h3 className="text-muted text-center">Login Form</h3>
+			<form className="forms form-margin" onSubmit={handleSubmit(onSubmit)} style={{marginLeft: "395px!important"}} >
+				< input name="email" ref={register({ required: true })} placeholder="Your Email" />
+				{errors.email && <span className="error">Email is required</span>}
+
+				<input name="password" ref={register({ required: true })} type="password" placeholder="Your Password" />
+				{errors.password && <span id="passErr" className="error">Password is required</span>}
+				<input type="submit" />
+				<p className="ml-4 mt-2 text-primary font-weight-bold" style={{cursor: "pointer"}} onClick={handleRegistration}>Create a new Account ? Please Sign Up</p>
+			</form >
+		</div>
+	)
+}
+
+export default LoginForm;
---
+++ client/src/components/pages/registration_form.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useHistory } from 'react-router';
+import { Input as InputField, message } from "antd";
+import './registration_form.css';
+
+const RegistrationForm = () => {
+	const { register, handleSubmit, control, errors } = useForm();
+	const history = useHistory();
+
+	const onSubmit = (data: any) => {
+		console.log(data);
+
+		fetch("http://localhost:5000/auth/signup", {
+			method: 'POST',
+			headers: {
+				'content-Type': 'application/json'
+			},
+			body: JSON.stringify(data),
+		})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data);
+			message.success('Registration Successfully!');
+			history.push('/login-form');
+		})
+		.catch(err => {
+			console.log(err);
+			message.error(err);
+		})
+	}
+	
+	const handleLogin = () => {
+      history.push('/login-form');
+    }
+
+	return (
+		<div>
+			<h3 className="text-muted text-center">Sign Up</h3>
+			<form className="forms form-margin" onSubmit={handleSubmit(onSubmit)}>
+				<input name="name" ref={register({ required: true })} placeholder="Your Name" />
+				{errors.name && <span className="error">Name is required</span>}
+
+				< input name="email" ref={register({ required: true })} placeholder="Your Email" />
+				{errors.email && <span className="error">Email is required</span>}
+
+				<input name="password" ref={register({ required: true })} type="password" placeholder="Your Password" />
+				{errors.password && <span id="passErr" className="error">Password is required</span>}
+				<input type="submit" />
+				<p className="ml-4 mt-2 text-primary font-weight-bold" style={{cursor: "pointer"}} onClick={handleLogin}>Already have an Account ? Please Log In</p>
+			</form>
+		</div>
+	)
+}
+
+export default RegistrationForm;
